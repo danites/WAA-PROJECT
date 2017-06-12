0adb689124dc4731d4b5fc38f14746157873aaa6
danites <kifledaniel7@gmail.com>
2017-06-11 22:57:14 -0500

    The last update

--- airlinesWebApp/src/main/java/edu/mum/cs545/ws/AirlineWebService.java
+++ airlinesWebApp/src/main/java/edu/mum/cs545/ws/AirlineWebService.java
@@ -9,7 +9,9 @@ import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import cs545.airline.model.Airline;
@@ -24,14 +26,16 @@ public class AirlineWebService {
 	@POST
 	@Path("/create")
 	@Produces({MediaType.APPLICATION_JSON})
+	@Consumes({MediaType.APPLICATION_JSON})
 	public void create(Airline airport) {
 		airlineService.create(airport);
 	}
 	@DELETE
-	@Path("/delete")
+	@Path("/{deleteId}")
 	@Consumes({MediaType.APPLICATION_JSON})
 	@Produces({MediaType.APPLICATION_JSON})
-	public void delete(Airline airport) {
+	public void delete(@PathParam("deleteId") long deleteId, Airline airport) {
+		airport.setId(deleteId);
 		airlineService.delete(airport);
 	}
 	@PUT
@@ -47,11 +51,12 @@ public class AirlineWebService {
 	public Airline find(Airline airline) {
 		return airlineService.find(airline);
 	}
+	
 	@GET
-	@Path("/findbyname")
+	@Path("findbyname")
 	@Produces({MediaType.APPLICATION_JSON})
-	public Airline findByName(String name) {
-		return airlineService.findByName(name);
+	public Airline findByName(@QueryParam("findbyname") String findbyname) {
+		return airlineService.findByName(findbyname);
 	}
 	@GET
 	@Path("/findbyflight")
